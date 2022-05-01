e53ab5598f9135c7a9c82d96bf4057b751ce6a81
Elias Santander <cazagb@gmail.com>
2022-05-01 16:27:57 -0300

    small tweaks

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,7 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req,res) => {
-    res.json({ message: 'get goals' })
+    res.json({ message: 'Get goals' })
+})
+
+router.post('/', (req,res) => {
+    res.json({ message: 'Set goals' })
+})
+
+router.put('/:id', (req,res) => {
+    res.json({ message: `Update ${req.params.id}` })
+})
+
+router.delete('/', (req,res) => {
+    res.json({ message: `Delete ${req.params.id}` })
 })
 
 module.exports = router
